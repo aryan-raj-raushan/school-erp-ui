d4e1f3a122f14984de492df1a9df1a753371b9e1
dharmendra <yaduvanshi.dhanno@gmail.com>
2026-06-08 04:32:34 +0530

    feat(holiday): implement UI and API integrations for Holiday-Events Page

--- src/app/(main)/school/holidays-events/[id]/page.tsx
+++ src/app/(main)/school/holidays-events/[id]/page.tsx
@@ -40,6 +40,7 @@ export default function SchoolEventDetailPage({
 }: {
   params: Promise<{ id: string }>;
 }) {
+  console.log("Params: ", params);
   const { id } = use(params);
   const router = useRouter();
   const searchParams = useSearchParams();
